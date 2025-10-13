b325d0a531c40faf8e500a8962add8b544aef6f5
shaikmaviya <shaik.mavi@gmail.com>
2025-10-13 20:21:41 +0530

    mm

---
+++ DSA With Pyhton/Arrays/move_zeros_to_the_end.py
@@ -0,0 +1,13 @@
+nums=[1,0,3,0,4,5]
+n=len(nums)
+count=0
+for i in range(n):
+    if nums[i] != 0:
+        nums[count]=nums[i]
+        count+=1
+while count<n:
+    nums[count]=0
+    count+=1
+        
+print(nums)
+                
