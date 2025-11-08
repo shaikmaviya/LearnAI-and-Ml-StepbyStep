7e655c4040b4064778470b047c9a4e50ecd5955c
shaikmaviya <shaik.mavi@gmail.com>
2025-11-08 10:27:28 +0530

    2025-11-08 10:27:28

--- DSA With Pyhton/hashing/number_hashing.py
+++ DSA With Pyhton/hashing/number_hashing.py
@@ -8,6 +8,10 @@ frequency_map = dict()
 
 for i in range(len(n)):
     if n[i] in frequency_map:
+        
+        
+        
+        
         frequency_map[n[i]] += 1
     else:
         frequency_map[n[i]] = 1
